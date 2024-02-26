613d1d28cdeb35fb34b0a6b299c5f60e902d0f68
Mahesh.Bhagat <mahesh.bhagat11@gmail.com>
2024-02-26 15:29:48 +0530

    09themeswitcher

---
+++ 09themeswitcher/src/components/Card.jsx
@@ -0,0 +1,20 @@
+export default function Card() {
+    return (
+        <>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg dark:bg-gray-800 dark:border-gray-700">
+            <img className="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2 dark:text-white">The Coldest Sunset</div>
+                <p className="text-gray-700 text-base dark:text-white">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
+        </div>
+        </>
+    );
+}
